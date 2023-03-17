74539f126df4f5cd25c89ba213a39d89a471df0b
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-03-17 17:57:30 +0100

    j'ai tout cassé :)

--- pbmd_tools.py
+++ pbmd_tools.py
@@ -3,10 +3,11 @@ import requests
 import xmltodict
 import dotenv
 import os
+import sys
 
 db = 'pubmed'
 domain = 'https://www.ncbi.nlm.nih.gov/entrez/eutils'
-nresults = 15000
+nresults = 15_000
 retmode = 'xml'
 
 def read_tokens():
@@ -19,19 +20,13 @@ def read_tokens():
         
     """
     dotenv.load_dotenv(".env")
-    if "GITHUB_KEY" in os.environ:
-        #print("Found GitHub token.")
-        git = os.environ.get("GITHUB_KEY")
-    else:
-        print("Token is missing.")
-    if "PUBMED_KEY" in os.environ:
-        #print("Found PubMed token.")
-        pub = os.environ.get("PUBMED_KEY")
-    else:
-        print("Token is missing.")
-    return pub, git
+    if "GITHUB_KEY" not in os.environ:
+        sys.exit("Cannot find Github token")
+    if "PUBMED_KEY" not in os.environ:
+        sys.exit("Cannot find PubMed token")
 
-def get_summary(PMID):
+
+def get_summary(PMID, token):
     """Obtaining information about an article published in PubMed using the PubMed API.
 
     Parameters
@@ -51,15 +46,170 @@ def get_summary(PMID):
     https://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=36540970&retmode=xml&rettype=abstract
     
     """
-    
-    API_KEY = read_tokens()[0]
-    queryLinkSearch = f'{domain}/efetch.fcgi?db={db}&id={PMID}&retmax={nresults}&retmode={retmode}&rettype=abstract&api_key={API_KEY}'  
+    queryLinkSearch = f'{domain}/efetch.fcgi?db={db}&id={PMID}&retmax={nresults}&retmode={retmode}&rettype=abstract&api_key={token}'  
     response = requests.get(queryLinkSearch)
     dic = xmltodict.parse(response.content)
-    
     return dic
 
-def get_info(PMID, log_error_sum):
+def get_abstract_from_summary(summary):
+    """Obtaining abstract from the dictionary with summary returned by PubMed API.
+
+    Parameters
+    ----------
+    summary : dictionary
+        A dictionary obtained from xml format provided by pubmed api entrez.
+
+    Returns
+    -------
+    abstract : str
+        The article abctact.
+    error : str
+        A list to stock the errors provided by this function.
+    """
+    error = []
+    
+    try:
+        article = res['PubmedArticleSet']['PubmedArticle']
+        abstract_raw = article['MedlineCitation']['Article']['Abstract']['AbstractText']
+        if isinstance(abstract_raw, list):
+            abstract = ""
+            for d in abstract_raw:
+                abstract += d['#text'] + " "    
+        elif isinstance(abstract_raw, dict):
+            abstract = ""
+            abstract += abstract_raw['#text'] + " "
+        else:
+            abstract = article['MedlineCitation']['Article']['Abstract']['AbstractText']  
+    except:
+        error.append(f"{PMID} : no abstract found")
+        
+    return abstract, error
+    
+def get_pubdate_from_summary(summary):
+    """Obtaining pubdate from the dictionary with summary returned by PubMed API.
+
+    Parameters
+    ----------
+    summary : dictionary
+        A dictionary obtained from xml format provided by pubmed api entrez.
+
+    Returns
+    -------
+    pubdate : str
+        The article abctact.
+    error : str
+        A list to stock the errors provided by this function.
+    """
+    error = []
+    
+    try:
+        article = res['PubmedArticleSet']['PubmedArticle']
+        date = article['MedlineCitation']['DateCompleted']
+        pubdate = date['Year'] + '-' + date['Month'] + '-' + date['Day']
+    except:
+        try:
+            article = res['PubmedArticleSet']['PubmedArticle']
+            date = article['MedlineCitation']['Article']['ArticleDate']
+            pubdate = date['Year'] + '-' + date['Month'] + '-' + date['Day']
+        except:
+            error.append(f"{PMID} : no publication date found")
+        
+    return pubdate, error
+
+def get_title_from_summary(summary):
+    """Obtaining title from the dictionary with summary returned by PubMed API.
+
+    Parameters
+    ----------
+    summary : dictionary
+        A dictionary obtained from xml format provided by pubmed api entrez.
+
+    Returns
+    -------
+    title : str
+        The article abctact.
+    error : str
+        A list to stock the errors provided by this function.
+    """
+    error = []
+    
+    try:
+        article = res['PubmedArticleSet']['PubmedArticle']
+        Tit = article['MedlineCitation']['Article']['ArticleTitle']
+        if isinstance(Tit, dict) or type(Tit) == "<class 'dict'>":
+            for d in Tit.keys():
+                title += Tit[d] + ' '
+        else:
+            title = Tit
+    except:
+        error.append(f"{PMID} : no title found")
+        
+    return title, error
+
+
+def get_journal_from_summary(summary):
+    """Obtaining journal name from the dictionary with summary returned by PubMed API.
+
+    Parameters
+    ----------
+    summary : dictionary
+        A dictionary obtained from xml format provided by pubmed api entrez.
+
+    Returns
+    -------
+    journal : str
+        The article abctact.
+    error : str
+        A list to stock the errors provided by this function.
+    """
+    error = []
+    
+    try:
+        article = res['PubmedArticleSet']['PubmedArticle']
+        journal = article['MedlineCitation']['Article']['Journal']['Title']
+    except:
+        error.append(f"{PMID} : no journal found")
+    
+    return journal, error
+
+def get_doi_from_summary(summary):
+    """Obtaining doi from the dictionary with summary returned by PubMed API.
+
+    Parameters
+    ----------
+    summary : dictionary
+        A dictionary obtained from xml format provided by pubmed api entrez.
+
+    Returns
+    -------
+    doi : str
+        The article abctact.
+    error : str
+        A list to stock the errors provided by this function.
+    """
+    error = []
+    
+    try:
+        try:
+            article = res['PubmedArticleSet']['PubmedArticle']
+            ELocationID_list = article['PubmedData']['ArticleIdList']['ArticleId']
+        except:
+            article = res['PubmedArticleSet']['PubmedArticle']
+            ELocationID_list = article['MedlineCitation']['Article']['ELocationID']
+        if isinstance(ELocationID_list, dict) or type(ELocationID_list) == "<class 'dict'>":
+            if "doi" in ELocationID_list.values():
+                doi = ELocationID_list["#text"]
+        else:
+            for dictionary in ELocationID_list:
+                if "doi" in dictionary.values():
+                    doi = dictionary["#text"]
+    except:
+        error.append(f"{PMID} : no DOI found")
+    
+    return journal, error
+    
+
+def get_info(PMID, token):
     """Obtaining information about an article published in PubMed using the PubMed API.
 
     Parameters
@@ -71,6 +221,8 @@ def get_info(PMID, log_error_sum):
 
     Returns
     -------
+    Abstract : str
+        The article abctact.
     PubDate : str
         The date of the article publication in the YYYY-MM-DD format.
     DOI : str
@@ -79,14 +231,12 @@ def get_info(PMID, log_error_sum):
         The journal name.
     Title : str
         The article title.
-    Abstract : str
-        The article abctact.
     log_error_sum : list
         The list with all the cases when the function have not managed to obtain one of the value listed above.
-        
     """
-    
-    res = get_summary(PMID)
+    info = {"abstract":"", "title":"", "journal":"", "publication_date":"", "doi":"", }
+    error = []
+    res = get_summary(PMID, token)
     
     # Abstract
     try:
@@ -95,50 +245,45 @@ def get_info(PMID, log_error_sum):
         if isinstance(abstract_raw, list):
             Abstract = ''
             for d in abstract_raw:
-                Abstract += d['#text'] + ' '    
+                info["abstract"] += d['#text'] + ' '    
         elif isinstance(abstract_raw, dict):
-            Abstract = ''
-            Abstract += abstract_raw['#text'] + ' '
+            info["abstract"] = ""
+            info["abstract"] += abstract_raw['#text'] + ' '
         else:
-            Abstract = article['MedlineCitation']['Article']['Abstract']['AbstractText']
-            
+            info["abstract"] = article['MedlineCitation']['Article']['Abstract']['AbstractText']  
     except:
-        Abstract = None
-        log_error_sum.append('No Abstract found :' + str(PMID) + '\n')
+        error.append(f"{PMID} : no abstract found")
         
     # PubDate
     try:
         article = res['PubmedArticleSet']['PubmedArticle']
         date = article['MedlineCitation']['DateCompleted']
-        PubDate = date['Year'] + '-' + date['Month'] + '-' + date['Day']
+        info["publication_date"] = date['Year'] + '-' + date['Month'] + '-' + date['Day']
     except:
         try:
             article = res['PubmedArticleSet']['PubmedArticle']
             date = article['MedlineCitation']['Article']['ArticleDate']
-            PubDate = date['Year'] + '-' + date['Month'] + '-' + date['Day']
+            info["publication_date"] = date['Year'] + '-' + date['Month'] + '-' + date['Day']
         except:
-            PubDate = None
-            log_error_sum.append('No Article Date found at all :' + str(PMID) + '\n')
+            error.append(f"{PMID} : no publication date found")
         
     # Title and Journal 
     try:
         article = res['PubmedArticleSet']['PubmedArticle']
         Tit = article['MedlineCitation']['Article']['ArticleTitle']
         if isinstance(Tit, dict) or type(Tit) == "<class 'dict'>":
-            Title = ''
             for d in Tit.keys():
-                Title += Tit[d] + ' '
+                info["title"] += Tit[d] + ' '
         else:
-            Title = Tit
+            info["title"] = Tit
     except:
-        Title = None
-        log_error_sum.append('No Title found :' + str(PMID) + '\n')
+        error.append(f"{PMID} : no title found")
+
     try:
         article = res['PubmedArticleSet']['PubmedArticle']
-        Journal = article['MedlineCitation']['Article']['Journal']['Title']
+        info["journal"] = article['MedlineCitation']['Article']['Journal']['Title']
     except:
-        Journal = None
-        log_error_sum.append('No Journal Name found :' + str(PMID) + '\n')
+        error.append(f"{PMID} : no journal found")
         
     # DOI
     DOI = None
@@ -151,18 +296,18 @@ def get_info(PMID, log_error_sum):
             ELocationID_list = article['MedlineCitation']['Article']['ELocationID']
         if isinstance(ELocationID_list, dict) or type(ELocationID_list) == "<class 'dict'>":
             if "doi" in ELocationID_list.values():
-                DOI = ELocationID_list["#text"]
+                info["doi"] = ELocationID_list["#text"]
         else:
             for dictionary in ELocationID_list:
                 #print(1)
                 if "doi" in dictionary.values():
                     #print(2)
-                    DOI = dictionary["#text"]
+                    info["doi"] = dictionary["#text"]
     except:
-        DOI = None
-        log_error_sum.append('No DOI found :' + str(PMID) + '\n')
+        error.append(f"{PMID} : no DOI found")
         
-    return PubDate, DOI, Journal, Title, Abstract, log_error_sum
+    return info, error
+
 
 def get_link(text):
     
@@ -186,31 +331,36 @@ def clean_link(links):
                 link = "https://" + link
             if link[-2] == ")":
                 link = link[:-2]
-            if link[-1] == ".":
+            if link[-1] == "." or link[-1] == ']' or link[-1] == '"':
                 link = link[:-1]
             if link[-4:] == '.git':
                 link = link[:-4]
             if link[-1] != "/":
-                links_fin += link+"/" + ' '
+                links_fin += link + "/" + ' '
             else:
                 links_fin += link + ' '
                 
     return links_fin
 
-def get_repo_info(link, log_error):
-    
-    owner = str(link).split('/')[3]
-    access_token = read_tokens()[1]
-    headers = {'Authorization':"Token "+access_token}
-    repo = str(link).split('/')[4]
-    url = f"https://api.github.com/repos/{owner}/{repo}"
 
-    response = requests.get(url,headers=headers)
+def get_repo_info(link, access_token):
+    """
+    Get GitHub repository info.
+    
+    Example: http://
+    """
+    owner = str(link).split('/')[3]
+    repo = str(link).split('/')[4]
+    
+    headers = {'Authorization': f"Token {access_token}"}
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+    
+    info = {"date_created": None, "date_updated": None, "errors": None}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         repository_info = response.json()
-        created_at = repository_info["created_at"]
-        updated_at = repository_info["updated_at"]
+        info["date_created"] = repository_info["created_at"].split("T")[0]
+        info["date_updated"] = updated_at = repository_info["updated_at"].split("T")[0]
     else:
-        log_error.append(f"Error with URL: {url} Status code: {response.status_code} ")
-        return None, None
-    return created_at.split('T')[0], updated_at.split('T')[0], log_error
+        info["errors"] = f"Error with URL: {url} Status code: {response.status_code} Answer: {response.json()}"
+    return info

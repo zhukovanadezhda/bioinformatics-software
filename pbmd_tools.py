41d74f7aed8f136133df214e5a507eecf2ebb81b
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-04-21 15:09:29 +0200

    software heritage verification

--- pbmd_tools.py
+++ pbmd_tools.py
@@ -16,10 +16,10 @@ def read_tokens():
         
     """
     dotenv.load_dotenv(".env")
-    if "GITHUB_KEY" not in os.environ:
-        sys.exit("Cannot find Github token")
-    if "PUBMED_KEY" not in os.environ:
-        sys.exit("Cannot find PubMed token")      
+    #if "GITHUB_KEY" not in os.environ:
+        #sys.exit("Cannot find Github token")
+    #if "PUBMED_KEY" not in os.environ:
+        #sys.exit("Cannot find PubMed token")      
         
 
 
